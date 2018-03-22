8eb58c99240256888cd207e18d3ff9c1f773e162
Anndo Ko <handcuffanndo@gmail.com>
2018-03-21 20:02:07 -0400

    Anndo: Add buttons for adjusting the font size

---
+++ js/common.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+  // Navigation
+  // Adjust the font size
+	$(".small-font").click(function(){
+		$("body").css("font-size","1em");
+	});
+	$(".normal-font").click(function(){
+		$("body").css("font-size","2em");
+	});
+  $(".big-font").click(function(){
+		$("body").css("font-size","3em");
+	});
+
+});
