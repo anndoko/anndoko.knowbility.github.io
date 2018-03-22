80607853b3a4e2db4eb80273c7026a0b017ce589
Anndo Ko <handcuffanndo@gmail.com>
2018-03-21 20:50:00 -0400

    Anndo: Add checkbox to enable high contrast

--- js/common.js
+++ js/common.js
@@ -1,15 +1,24 @@
 $(document).ready(function() {
 
-  // Navigation
+  // Enable high contrast
+  $("#highContrastEnable").on("change", function(){
+    if (this.checked){
+      $("*").addClass("high-contrast");
+    }
+    else{
+      $("*").removeClass("high-contrast");
+    }
+  });
+
   // Adjust the font size
 	$(".small-font").click(function(){
-		$("body").css("font-size","1em");
+		$("body").css("font-size","12px");
 	});
 	$(".normal-font").click(function(){
-		$("body").css("font-size","2em");
+		$("body").css("font-size","16px");
 	});
   $(".big-font").click(function(){
-		$("body").css("font-size","3em");
+		$("body").css("font-size","24px");
 	});
 
 });
